12792e0f487adf5d47bde34eeffb660229754766
Ubeyt <190541075@firat.edu.tr>
2021-08-22 15:09:58 +0300

    updates cors

--- src/middlewares/cors/cors.ts
+++ src/middlewares/cors/cors.ts
@@ -1,5 +1,7 @@
 import cors from 'cors';
-
+const { CORS_WHITE_LIST } = process.env;
+const whiteList =
+  typeof CORS_WHITE_LIST === 'string' ? CORS_WHITE_LIST.split(',') : [];
 const options: cors.CorsOptions = {
   allowedHeaders: [
     'Origin',
@@ -10,10 +12,16 @@ const options: cors.CorsOptions = {
   ],
   credentials: true,
   methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-  origin:
-    process.env.NODE_ENV === 'production'
-      ? process.env.FRONTEND_PRODUCTION
-      : process.env.FRONTEND_DEV,
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, origin?: string | undefined) => void
+  ) => {
+    if (whiteList.indexOf(origin as string) !== -1) {
+      callback(null, origin);
+    } else {
+      callback(new Error(`Your access has been blocked by CORS.`));
+    }
+  },
   preflightContinue: false,
 };
 
